2557a82e20233bb8cf594b22c8c1a6710bfbc34a
Bobby Yang <bobstermyang@gmail.com>
2014-10-24 21:40:40 +0000

    First commit

---
+++ app/models/generate_slug.rb
@@ -0,0 +1,14 @@
+class Link < ActiveRecord::Base
+ def generate_slug
+    self.slug = self.id.to_s(36)
+    self.save
+  end
+ def display_slug
+    ENV['BASE_URL'] + self.slug
+  end
+ def screenshot_scrape
+    Screenshot.perform_async(self.id)
+    Scrape.perform_async(self.id)
+  end
+after_create :generate_slug, :screenshot_scrape
+end
